68d7052f59116d6bfaa585f19032daccfcdf0cd2
charanm08 <mekakrishnacharan@gmail.com>
2025-01-20 16:27:37 +0530

    fix static routes override

--- f5_cccl/service/manager.py
+++ f5_cccl/service/manager.py
@@ -484,10 +484,8 @@ class ServiceConfigDeployer(object):
         desired = desired_config.get('routes', dict())
         LOGGER.debug("Getting route tasks...Desired-1")
         LOGGER.debug(desired['k8s-selected-jennet-cluster-1-master-10.4.1.42'])
-        LOGGER.debug("Getting route tasks...Desired-2")
+        LOGGER.debug(type(desired['k8s-selected-jennet-cluster-1-master-10.4.1.42']))
         LOGGER.debug(dir(desired['k8s-selected-jennet-cluster-1-master-10.4.1.42']))
-        LOGGER.debug("Getting route tasks...Desired-3")
-        LOGGER.debug(desired['k8s-selected-jennet-cluster-1-master-10.4.1.42']['description'])
         LOGGER.debug("Getting route tasks...Desired-4")
         LOGGER.debug(existing['k8s-selected-jennet-cluster-1-master-10.4.1.42']['description'])
         #description = desired['0']
